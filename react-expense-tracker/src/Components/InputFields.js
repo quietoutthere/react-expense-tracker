f31b79565a1632223d41e0cb72de8b328c096d6a
Evan Oyster <oyster.evan@gmail.com>
2022-12-02 10:12:20 -0500

    inpufields value warning

--- react-expense-tracker/src/Components/InputFields.js
+++ react-expense-tracker/src/Components/InputFields.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
 function InputFields() {
+
+    const handleClick = () => {
+        console.log('noice');
+    }
+
+
     return (
        <form>
             <div className="input-fields">
                 <label htmlFor="type">Type: </label>
                 <select name="type" id="type" required>
-                    <option disabled selected value="">Select</option>
+                    <option disabled selected value=''>Select</option>
                     <option value="card">Card</option>
                     <option value="cash">Cash</option>
                     <option value="other">Other</option>
@@ -17,6 +23,9 @@ function InputFields() {
                 <input type="date" required />
                 <label htmlFor="amount">Amount: </label>
                 <input type="number" placeholder='What did it cost?' step={.01} required/>
+            </div>
+            <div>
+                <button onClick={handleClick}>Run Expenses</button>
             </div>
        </form>
     )
