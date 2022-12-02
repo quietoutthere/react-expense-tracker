facb119b91e08c8d5656dcae9729e06264fff0b1
Evan Oyster <oyster.evan@gmail.com>
2022-12-02 09:29:29 -0500

    InputFields update

--- react-expense-tracker/src/App.js
+++ react-expense-tracker/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import './Components/InputFields'
 import InputFields from './Components/InputFields';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
+        <Header />
         <InputFields />
     </div>
   );
--- react-expense-tracker/src/Components/InputFields.js
+++ react-expense-tracker/src/Components/InputFields.js
@@ -2,7 +2,23 @@ import React from 'react';
 
 function InputFields() {
     return (
-       <h1>Hello</h1>
+       <form>
+            <div className="input-fields">
+                <label htmlFor="type">Type: </label>
+                <select name="type" id="type" required>
+                    <option disabled selected value="">Select</option>
+                    <option value="card">Card</option>
+                    <option value="cash">Cash</option>
+                    <option value="other">Other</option>
+                </select>
+                <label htmlFor="vendor">Vendor: </label>
+                <input type="text" placeholder='What did you spend it on?' required/>
+                <label htmlFor="date">Date: </label>
+                <input type="date" required />
+                <label htmlFor="amount">Amount: </label>
+                <input type="number" placeholder='What did it cost?' step={.01} required/>
+            </div>
+       </form>
     )
 };
 
