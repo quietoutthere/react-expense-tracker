fb59f48229fd817a7dadacb8a6e30c304d2ef15c
Evan Oyster <oyster.evan@gmail.com>
2022-12-07 09:51:24 -0500

    getting button to log expenses

--- react-expense-tracker/src/Components/InputFields.js
+++ react-expense-tracker/src/Components/InputFields.js
@@ -10,10 +10,8 @@ function InputFields() {
     ]);
    
 
-    const handleClick = (event) => {
-        
+    const handleClick = () => {
     }
-
 
     return (
         <>
@@ -22,9 +20,9 @@ function InputFields() {
                     <label htmlFor="type">Type: </label>
                     <select name="type" id="type" defaultValue={ 'Select' } value={ input.type } onChange={e => setInput({ ...input, type: e.target.value })} required>
                         <option disabled defaultValue={ 'Select' }>Select</option>
-                        <option value={ 'card' }>Card</option>
-                        <option value={ 'cash' }>Cash</option>
-                        <option value={ 'other' }>Other</option>
+                        <option value={ 'Card' }>Card</option>
+                        <option value={ 'Cash' }>Cash</option>
+                        <option value={ 'Other' }>Other</option>
                     </select>
                     <label htmlFor="vendor" >Vendor: </label>
                     <input type="text" placeholder="Name of Vendor" value={input.vendor} onChange={e => setInput({ ...input, vendor: e.target.value })} required />
@@ -34,7 +32,7 @@ function InputFields() {
                     <input type="number" placeholder='What did it cost?' step={ .01 } value={input.amount} onChange={e => setInput({ ...input, amount: e.target.value})} required/>
                 </div>
                 <div>
-                    <button onClick={handleClick}>Run Expenses</button>
+                    <button onClick={handleClick}>Log Expense</button>
                 </div>
             </form>
             <div>
